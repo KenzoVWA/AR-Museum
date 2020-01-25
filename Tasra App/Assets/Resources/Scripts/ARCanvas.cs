c44b2b65295ea3779fe10e2550a0b7c96f7852ab
Kenzo Van Waerebeek Alfaro <32316281+KenzoVWA@users.noreply.github.com>
2020-01-25 12:41:58 -0500

    minor corrections
    added sound for test on elephant

--- Tasra App/Assets/Resources/Scripts/ARCanvas.cs
+++ Tasra App/Assets/Resources/Scripts/ARCanvas.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TestScript : MonoBehaviour
+public class ARCanvas : MonoBehaviour
 {
     public GameObject panel;
     public GameObject information;
